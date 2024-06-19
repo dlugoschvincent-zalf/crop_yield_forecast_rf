c072848590c379d87b63cd6464b8d0fa17ff644e
Vincent Dlugosch <vincent.dlugosch@zalf.de>
2024-06-19 09:16:35 +0200

    feat: add small script to combine and save all the years into one file for easier processing

---
+++ src/combine_all_years_pr.py
@@ -0,0 +1,28 @@
+import xarray as xr
+
+ds_path_list = [
+    "../climate_data/amber/2007/zalf_pr_amber_2007_v1-0.nc",
+    "../climate_data/amber/2008/zalf_pr_amber_2008_v1-0.nc",
+    "../climate_data/amber/2009/zalf_pr_amber_2009_v1-0.nc",
+    "../climate_data/amber/2010/zalf_pr_amber_2010_v1-0.nc",
+    "../climate_data/amber/2011/zalf_pr_amber_2011_v1-0.nc",
+    "../climate_data/amber/2012/zalf_pr_amber_2012_v1-0.nc",
+    "../climate_data/amber/2013/zalf_pr_amber_2013_v1-0.nc",
+    "../climate_data/amber/2014/zalf_pr_amber_2014_v1-0.nc",
+    "../climate_data/amber/2015/zalf_pr_amber_2015_v1-0.nc",
+    "../climate_data/amber/2016/zalf_pr_amber_2016_v1-0.nc",
+    "../climate_data/amber/2017/zalf_pr_amber_2017_v1-0.nc",
+    "../climate_data/amber/2018/zalf_pr_amber_2018_v1-0.nc",
+    "../climate_data/amber/2019/zalf_pr_amber_2019_v1-0.nc",
+    "../climate_data/amber/2020/zalf_pr_amber_2020_v1-0.nc",
+    "../climate_data/amber/2021/zalf_pr_amber_2021_v1-0.nc",
+    "../climate_data/amber/2022/zalf_pr_amber_2022_v1-0.nc",
+    "../climate_data/amber/2023/zalf_pr_amber_2023_v1-0.nc",
+]
+ds_list = []
+for ds_path in ds_path_list:
+    ds_list.append(xr.open_dataset(ds_path))
+ds = xr.concat(ds_list, dim="time")
+
+ds.to_netcdf("allyears_uncompressed.nc", encoding={"pr": {"zlib": False}})
+print("saved")
